4eb46d9e9f722ea5718aa2ccac236e8c1b4e002e
Samuel Bertrand <bertsa.pro@gmail.com>
2022-04-26 23:54:46 -0400

    Test

--- version-bump.sh
+++ version-bump.sh
@@ -55,6 +55,8 @@ else
   echo "pom.xml at" $POMPATH "will be bumped from" $OLD_VERSION "to" $NEW_VERSION
   mvn -q versions:set -DnewVersion="${NEW_VERSION}"
   git add $POMPATH/pom.xml
+  cd "./$PACKAGEJSONPATH" && npm version "$NEW_VERSION" --no-git-tag-version --allow-same-version
+  git add "$PACKAGEJSONPATH"/package.json
   REPO="https://$GITHUB_ACTOR:$TOKEN@github.com/$GITHUB_REPOSITORY.git"
   git commit -m "Bump pom.xml from $OLD_VERSION to $NEW_VERSION"
   git tag $NEW_VERSION
